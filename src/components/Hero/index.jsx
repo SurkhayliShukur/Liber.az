bdbd3d606dc5df99d5b2a511cf3afbb365511688
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-10 19:21:06 +0400

    responsive

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,23 +7,20 @@ const Hero = () => {
         <>
             <Header />
             <section className='relative h-full'>
-                <div className="grid place-items-center">
-                    <img src="https://images.unsplash.com/photo-1578265707481-77c0ac3ce8bf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className='w-full h-auto' />
-                    <div className='absolute'>
-                        <p className=' bg-zinc-600 bg-opacity-50 text-white text-2xl' data-aos="fade-down">
-                            <span>
-                                From Skyline to Shoreline
-                            </span>
-                        </p>
-                    </div>
-                    <div  className='absolute pt-28' >
-                        <h1 data-aos="fade-up" className='text-8xl text-bold text-white'>
-                            Luxury Transportation Concierge
-                        </h1>
-                    </div>
-
+            <div className="grid place-items-center">
+                <img src="https://images.unsplash.com/photo-1578265707481-77c0ac3ce8bf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className='w-full h-auto' />
+                <div className='absolute text-center inset-0 flex flex-col justify-center items-center'>
+                    <p className='bg-zinc-600 bg-opacity-50 text-white text-2xl md:text-3xl lg:text-4xl' data-aos="fade-down">
+                        <span>
+                            From Skyline to Shoreline
+                        </span>
+                    </p>
+                    <h1 data-aos="fade-up" className='text-4xl md:text-6xl lg:text-8xl font-bold text-white mt-4 md:mt-8'>
+                        Luxury Transportation Concierge
+                    </h1>
                 </div>
-            </section>
+            </div>
+        </section>
         </>
     )
 }
