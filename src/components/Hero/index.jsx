5da962e72c761214f78f9d8e6d53216ff9840aa9
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-10 19:22:19 +0400

    save

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className="grid place-items-center">
                 <img src="https://images.unsplash.com/photo-1578265707481-77c0ac3ce8bf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className='w-full h-auto' />
                 <div className='absolute text-center inset-0 flex flex-col justify-center items-center'>
-                    <p className='bg-zinc-600 bg-opacity-50 text-white text-2xl md:text-3xl lg:text-4xl' data-aos="fade-down">
+                    <p className='bg-zinc-600 bg-opacity-50 text-white text-xl md:text-3xl lg:text-4xl' data-aos="fade-down">
                         <span>
                             From Skyline to Shoreline
                         </span>
