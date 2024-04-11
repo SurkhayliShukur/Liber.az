5b95afab29c6c1437f597b41dd1d9bdadab6087b
Shukur939 <shukursurkhayli000@gmail.com>
2024-04-11 18:41:27 +0400

    app

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,9 @@ function App() {
 
 
         <section className="section_car min-h-screen pb-24 pt-24 overflow-hidden">
-          <div className="container flex flex-col lg:flex-row items-center justify-center lg:justify-between">
-            <div data-aos="fade-down" className="lg:w-[500px] p-8 z-10">
-              <img src="https://plus.unsplash.com/premium_photo-1681489433354-f7fabdebd933?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="w-full h-[400px] object-cover" />
+          <div className="container flex flex-col lg:flex-row items-center justify-center">
+            <div data-aos="fade-down" className="lg:w-full sm:w-[100px] p-8 z-10 flex-shrink-0">
+              <img src="https://plus.unsplash.com/premium_photo-1681489433354-f7fabdebd933?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="lg:w-full lg:h-[400px] object-cover sm:w-[250px] sm:h-[250px]" />
             </div>
             <div data-aos="fade-down" className="text-center lg:text-left mt-8 lg:mt-0 z-10">
               <h4 className="text-2xl lg:text-3xl text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, quos?</h4>
